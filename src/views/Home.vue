8398bbea552501910cc538d2dae714805314a4f9
dingyang <mrding0815@163.com>
2020-06-23 10:27:56 +0800

    登录注册完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,42 @@
 <template>
   <div class="homeContent">
-    <router-link to="/login">登录页</router-link>
+    <router-link to="/login">登录页</router-link> -->
+    <router-link to="/register">注册页</router-link>
+    <router-link to="/personal">个人中心</router-link>
+    <header>
+      <div class="homeLogo">
+        <span class="iconfont iconnew"></span>
+      </div>
+    </header>
   </div>
 </template>
 
 <script>
 export default {
-  components: {
-  }
+  components: {}
 };
 </script>
 <style lang="less">
+header {
+  height: 13.61vw;
+  width: 100vw;
+  background-color: rgb(255, 0, 0);
+  position: relative;
+  .homeLogo {
+    color: rgb(255, 255, 255);
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 13.61vw;
+    height: 13.61vw;
+    margin-left: 7.78vw;                                                                            
+    span {
+      display: block;
+      width: 13.61vw;
+      height: 13.61vw;
+      line-height: 13.61vw;
+      font-size: 13.61vw;
+    }
+  }
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,21 +6,23 @@
     <div class="loginLogo">
       <span class="iconfont iconnew"></span>
     </div>
-    <AuthPage 
-    placeholderText="手机号" 
-    inputType="text" 
-    rule="0?(13|15|17|18)[0-9]{9}$"
-    errMsg="请输入合法的手机号！"
-    @sendMsg="logMObilePhone"
+    <AuthPage
+      placeholderText="手机号"
+      inputType="text"
+      rule="0?(13|15|17|18)[0-9]{9}$"
+      errMsg="请输入合法的手机号！"
+      @sendMsg="logMObilePhone"
+      @checkInput="checkNum"
     />
-    <AuthPage 
-    placeholderText="密码" 
-    inputType="password" 
-    rule="^.{3,11}$"
-    errMsg="您输入的密码有误，请重新输入！"
-    @sendMsg="logPwd"
+    <AuthPage
+      placeholderText="密码"
+      inputType="password"
+      rule="^.{3,11}$"
+      errMsg="您输入的密码有误，请重新输入！"
+      @sendMsg="logPwd"
+      @checkInput="checkPwd"
     />
-    <AuthBtn btnVal="登录" @click.native="login"/>
+    <AuthBtn btnVal="登录" @click.native="login" />
   </div>
 </template>
 
@@ -33,33 +35,49 @@ export default {
     AuthPage,
     AuthBtn
   },
-  data(){
-    return{
-      logUsername:'',
-      logPassword:''
+  data() {
+    return {
+      logUsername: "",
+      logPassword: "",
+      checkA:false,
+      checkB:false
+    };
+  },
+  methods: {
+    logMObilePhone(msg) {
+      this.logUsername = msg;
+    },
+    logPwd(msg) {
+      this.logPassword = msg;
+    },
+    checkNum(i){
+      this.checkA = i;
+    },
+    checkPwd(i){
+      this.checkB = i;
+    },
+    login() {
+      if(!(this.checkA&&this.checkB)){
+          return false;
+        }
+      this.$axios({
+        url: "http://127.0.0.1:3000/login",
+        method: "post",
+        data: {
+          username: this.logUsername,
+          password: this.logPassword
+        }
+      }).then(res => {
+        console.log(res.data);
+        const { message, data } = res.data;
+        if (message === "登录成功") {
+          this.$toast.success(message);
+          this.$router.push({ path: "/" });
+        } else {
+          this.$toast.fail('用户名或密码错误！');
+        }
+      });
     }
-  },
-  methods:{
-  logMObilePhone(msg){
-    this.logUsername = msg;
-  },
-  logPwd(msg){
-    this.logPassword = msg;
-  },
-  login(){
-    console.log(1111);
-    
-    this.$axios({
-      url:"http://127.0.0.1:3000/login",
-      method:"post",
-      data:{
-        username:this.logUsername,
-        password:this.logPassword
-      }
-    }).then(res=>{
-      this.$toast.success("登录成功");
-    })
-  }
   }
 };
 </script>
